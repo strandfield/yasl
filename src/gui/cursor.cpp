8a4823dfa7b351715c945448894cde233308964c
RugessNome <vincent.chambrin@gmail.com>
2018-08-21 17:58:13 +0200

    [gui] binding QCursor using yasl-meta

--- src/gui/cursor.cpp
+++ src/gui/cursor.cpp
@@ -7,19 +7,11 @@
 #include "yasl/binding/class.h"
 #include "yasl/binding/namespace.h"
 
-#include "yasl/core/datastream.h"
 #include "yasl/core/enums.h"
 #include "yasl/core/point.h"
+#include "yasl/gui/cursor.h"
 
-#include "yasl/gui/bitmap.h"
-#include "yasl/gui/pixmap.h"
-#include "yasl/gui/screen.h"
-
-#include <script/class.h>
 #include <script/classbuilder.h>
-#include <script/namespace.h>
-
-#include <QDebug>
 
 static void register_cursor_class(script::Namespace ns)
 {
@@ -29,18 +21,18 @@ static void register_cursor_class(script::Namespace ns)
 
   binding::Class<QCursor> binder{ cursor };
 
-  // ~QCursor();
-  binder.add_dtor();
   // QCursor();
   binder.ctors().add_default();
   // QCursor(Qt::CursorShape);
   binder.ctors().add<Qt::CursorShape>();
   // QCursor(const QBitmap &, const QBitmap &, int, int);
-  binder.ctors().add<const QBitmap &, const QBitmap &, int, int>();
+  /// TODO: QCursor(const QBitmap &, const QBitmap &, int, int);
   // QCursor(const QPixmap &, int, int);
-  binder.ctors().add<const QPixmap &, int, int>();
+  /// TODO: QCursor(const QPixmap &, int, int);
   // QCursor(const QCursor &);
   binder.ctors().add<const QCursor &>();
+  // ~QCursor();
+  binder.add_dtor();
   // QCursor & operator=(const QCursor &);
   binder.operators().assign<const QCursor &>();
   // QCursor(QCursor &&);
@@ -54,33 +46,36 @@ static void register_cursor_class(script::Namespace ns)
   // void setShape(Qt::CursorShape);
   binder.add_void_fun<Qt::CursorShape, &QCursor::setShape>("setShape");
   // const QBitmap * bitmap() const;
-  binder.add_fun<const QBitmap *, &QCursor::bitmap>("bitmap");
+  /// TODO: const QBitmap * bitmap() const;
   // const QBitmap * mask() const;
-  binder.add_fun<const QBitmap *, &QCursor::mask>("mask");
+  /// TODO: const QBitmap * mask() const;
   // QPixmap pixmap() const;
-  binder.add_fun<QPixmap, &QCursor::pixmap>("pixmap");
+  /// TODO: QPixmap pixmap() const;
   // QPoint hotSpot() const;
   binder.add_fun<QPoint, &QCursor::hotSpot>("hotSpot");
   // static QPoint pos();
   binder.add_static<QPoint, &QCursor::pos>("pos");
   // static QPoint pos(const QScreen *);
-  binder.add_static<QPoint, const QScreen *, &QCursor::pos>("pos");
+  /// TODO: static QPoint pos(const QScreen *);
   // static void setPos(int, int);
   binder.add_static_void_fun<int, int, &QCursor::setPos>("setPos");
   // static void setPos(QScreen *, int, int);
-  binder.add_static_void_fun<QScreen *, int, int, &QCursor::setPos>("setPos");
+  /// TODO: static void setPos(QScreen *, int, int);
   // static void setPos(const QPoint &);
   binder.add_static_void_fun<const QPoint &, &QCursor::setPos>("setPos");
   // static void setPos(QScreen *, const QPoint &);
-  binder.add_static_void_fun<QScreen *, const QPoint &, &QCursor::setPos>("setPos");
+  /// TODO: static void setPos(QScreen *, const QPoint &);
 }
 
-void register_cursor_file(script::Namespace root)
+
+void register_cursor_file(script::Namespace gui)
 {
   using namespace script;
 
-  register_cursor_class(root);
-  binding::Namespace binder{ root };
+  Namespace ns = gui;
+
+  register_cursor_class(ns);
+  binding::Namespace binder{ ns };
 
   // void swap(QCursor &, QCursor &);
   binder.add_void_fun<QCursor &, QCursor &, &swap>("swap");
@@ -89,10 +84,10 @@ void register_cursor_file(script::Namespace root)
   // bool operator!=(const QCursor &, const QCursor &);
   binder.operators().neq<const QCursor &, const QCursor &>();
   // QDataStream & operator<<(QDataStream &, const QCursor &);
-  binder.operators().put_to<QDataStream &, const QCursor &>();
+  /// TODO: QDataStream & operator<<(QDataStream &, const QCursor &);
   // QDataStream & operator>>(QDataStream &, QCursor &);
-  binder.operators().read_from<QDataStream &, QCursor &>();
+  /// TODO: QDataStream & operator>>(QDataStream &, QCursor &);
   // QDebug operator<<(QDebug, const QCursor &);
-  binder.operators().left_shift<QDebug, QDebug, const QCursor &>();
+  /// TODO: QDebug operator<<(QDebug, const QCursor &);
 }
 
