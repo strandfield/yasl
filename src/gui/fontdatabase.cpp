98c2297550bab40e0d40c2da3e57600865899771
RugessNome <vincent.chambrin@gmail.com>
2018-08-21 17:51:07 +0200

    [gui] binding QFontDatabase using yasl-meta

--- src/gui/fontdatabase.cpp
+++ src/gui/fontdatabase.cpp
@@ -9,28 +9,11 @@
 #include "yasl/binding/namespace.h"
 
 #include "yasl/core/bytearray.h"
-#include "yasl/core/list.h"
-#include "yasl/core/string.h"
-
 #include "yasl/gui/font.h"
-#include "yasl/gui/fontinfo.h"
+#include "yasl/gui/fontdatabase.h"
 
-#include <script/class.h>
 #include <script/classbuilder.h>
 #include <script/enumbuilder.h>
-#include <script/namespace.h>
-
-static void register_font_database_system_font_enum(script::Class font_database)
-{
-  using namespace script;
-
-  Enum system_font = font_database.Enum("SystemFont").setId(script::Type::QFontDatabaseSystemFont).get();
-
-  system_font.addValue("FixedFont", QFontDatabase::FixedFont);
-  system_font.addValue("GeneralFont", QFontDatabase::GeneralFont);
-  system_font.addValue("SmallestReadableFont", QFontDatabase::SmallestReadableFont);
-  system_font.addValue("TitleFont", QFontDatabase::TitleFont);
-}
 
 static void register_font_database_writing_system_enum(script::Class font_database)
 {
@@ -39,42 +22,56 @@ static void register_font_database_writing_system_enum(script::Class font_databa
   Enum writing_system = font_database.Enum("WritingSystem").setId(script::Type::QFontDatabaseWritingSystem).get();
 
   writing_system.addValue("Any", QFontDatabase::Any);
-  writing_system.addValue("Arabic", QFontDatabase::Arabic);
-  writing_system.addValue("Armenian", QFontDatabase::Armenian);
-  writing_system.addValue("Bengali", QFontDatabase::Bengali);
-  writing_system.addValue("Cyrillic", QFontDatabase::Cyrillic);
-  writing_system.addValue("Devanagari", QFontDatabase::Devanagari);
-  writing_system.addValue("Georgian", QFontDatabase::Georgian);
-  writing_system.addValue("Greek", QFontDatabase::Greek);
-  writing_system.addValue("Gujarati", QFontDatabase::Gujarati);
-  writing_system.addValue("Gurmukhi", QFontDatabase::Gurmukhi);
-  writing_system.addValue("Hebrew", QFontDatabase::Hebrew);
-  writing_system.addValue("Japanese", QFontDatabase::Japanese);
-  writing_system.addValue("Kannada", QFontDatabase::Kannada);
-  writing_system.addValue("Khmer", QFontDatabase::Khmer);
-  writing_system.addValue("Korean", QFontDatabase::Korean);
-  writing_system.addValue("Lao", QFontDatabase::Lao);
   writing_system.addValue("Latin", QFontDatabase::Latin);
-  writing_system.addValue("Malayalam", QFontDatabase::Malayalam);
-  writing_system.addValue("Myanmar", QFontDatabase::Myanmar);
-  writing_system.addValue("Nko", QFontDatabase::Nko);
-  writing_system.addValue("Ogham", QFontDatabase::Ogham);
-  writing_system.addValue("Oriya", QFontDatabase::Oriya);
-  writing_system.addValue("Other", QFontDatabase::Other);
-  writing_system.addValue("Runic", QFontDatabase::Runic);
-  writing_system.addValue("SimplifiedChinese", QFontDatabase::SimplifiedChinese);
-  writing_system.addValue("Sinhala", QFontDatabase::Sinhala);
-  writing_system.addValue("Symbol", QFontDatabase::Symbol);
+  writing_system.addValue("Greek", QFontDatabase::Greek);
+  writing_system.addValue("Cyrillic", QFontDatabase::Cyrillic);
+  writing_system.addValue("Armenian", QFontDatabase::Armenian);
+  writing_system.addValue("Hebrew", QFontDatabase::Hebrew);
+  writing_system.addValue("Arabic", QFontDatabase::Arabic);
   writing_system.addValue("Syriac", QFontDatabase::Syriac);
+  writing_system.addValue("Thaana", QFontDatabase::Thaana);
+  writing_system.addValue("Devanagari", QFontDatabase::Devanagari);
+  writing_system.addValue("Bengali", QFontDatabase::Bengali);
+  writing_system.addValue("Gurmukhi", QFontDatabase::Gurmukhi);
+  writing_system.addValue("Gujarati", QFontDatabase::Gujarati);
+  writing_system.addValue("Oriya", QFontDatabase::Oriya);
   writing_system.addValue("Tamil", QFontDatabase::Tamil);
   writing_system.addValue("Telugu", QFontDatabase::Telugu);
-  writing_system.addValue("Thaana", QFontDatabase::Thaana);
+  writing_system.addValue("Kannada", QFontDatabase::Kannada);
+  writing_system.addValue("Malayalam", QFontDatabase::Malayalam);
+  writing_system.addValue("Sinhala", QFontDatabase::Sinhala);
   writing_system.addValue("Thai", QFontDatabase::Thai);
+  writing_system.addValue("Lao", QFontDatabase::Lao);
   writing_system.addValue("Tibetan", QFontDatabase::Tibetan);
+  writing_system.addValue("Myanmar", QFontDatabase::Myanmar);
+  writing_system.addValue("Georgian", QFontDatabase::Georgian);
+  writing_system.addValue("Khmer", QFontDatabase::Khmer);
+  writing_system.addValue("SimplifiedChinese", QFontDatabase::SimplifiedChinese);
   writing_system.addValue("TraditionalChinese", QFontDatabase::TraditionalChinese);
+  writing_system.addValue("Japanese", QFontDatabase::Japanese);
+  writing_system.addValue("Korean", QFontDatabase::Korean);
   writing_system.addValue("Vietnamese", QFontDatabase::Vietnamese);
+  writing_system.addValue("Symbol", QFontDatabase::Symbol);
+  writing_system.addValue("Other", QFontDatabase::Other);
+  writing_system.addValue("Ogham", QFontDatabase::Ogham);
+  writing_system.addValue("Runic", QFontDatabase::Runic);
+  writing_system.addValue("Nko", QFontDatabase::Nko);
   writing_system.addValue("WritingSystemsCount", QFontDatabase::WritingSystemsCount);
 }
+
+
+static void register_font_database_system_font_enum(script::Class font_database)
+{
+  using namespace script;
+
+  Enum system_font = font_database.Enum("SystemFont").setId(script::Type::QFontDatabaseSystemFont).get();
+
+  system_font.addValue("GeneralFont", QFontDatabase::GeneralFont);
+  system_font.addValue("FixedFont", QFontDatabase::FixedFont);
+  system_font.addValue("TitleFont", QFontDatabase::TitleFont);
+  system_font.addValue("SmallestReadableFont", QFontDatabase::SmallestReadableFont);
+}
+
 
 static void register_font_database_class(script::Namespace ns)
 {
@@ -82,32 +79,30 @@ static void register_font_database_class(script::Namespace ns)
 
   Class font_database = ns.Class("FontDatabase").setId(script::Type::QFontDatabase).get();
 
-  register_font_database_system_font_enum(font_database);
   register_font_database_writing_system_enum(font_database);
+  register_font_database_system_font_enum(font_database);
   binding::Class<QFontDatabase> binder{ font_database };
 
-  // ~QFontDatabase();
-  binder.add_dtor();
   // static QList<int> standardSizes();
-  binder.add_static<QList<int>, &QFontDatabase::standardSizes>("standardSizes");
+  /// TODO: static QList<int> standardSizes();
   // QFontDatabase();
   binder.ctors().add_default();
   // QList<QFontDatabase::WritingSystem> writingSystems() const;
-  binder.add_fun<QList<QFontDatabase::WritingSystem>, &QFontDatabase::writingSystems>("writingSystems");
+  /// TODO: QList<QFontDatabase::WritingSystem> writingSystems() const;
   // QList<QFontDatabase::WritingSystem> writingSystems(const QString &) const;
-  binder.add_fun<QList<QFontDatabase::WritingSystem>, const QString &, &QFontDatabase::writingSystems>("writingSystems");
+  /// TODO: QList<QFontDatabase::WritingSystem> writingSystems(const QString &) const;
   // QStringList families(QFontDatabase::WritingSystem) const;
-  binder.add_fun<QStringList, QFontDatabase::WritingSystem, &QFontDatabase::families>("families");
+  /// TODO: QStringList families(QFontDatabase::WritingSystem) const;
   // QStringList styles(const QString &) const;
-  binder.add_fun<QStringList, const QString &, &QFontDatabase::styles>("styles");
+  /// TODO: QStringList styles(const QString &) const;
   // QList<int> pointSizes(const QString &, const QString &);
-  binder.add_fun<QList<int>, const QString &, const QString &, &QFontDatabase::pointSizes>("pointSizes");
+  /// TODO: QList<int> pointSizes(const QString &, const QString &);
   // QList<int> smoothSizes(const QString &, const QString &);
-  binder.add_fun<QList<int>, const QString &, const QString &, &QFontDatabase::smoothSizes>("smoothSizes");
+  /// TODO: QList<int> smoothSizes(const QString &, const QString &);
   // QString styleString(const QFont &);
   binder.add_fun<QString, const QFont &, &QFontDatabase::styleString>("styleString");
   // QString styleString(const QFontInfo &);
-  binder.add_fun<QString, const QFontInfo &, &QFontDatabase::styleString>("styleString");
+  /// TODO: QString styleString(const QFontInfo &);
   // QFont font(const QString &, const QString &, int) const;
   binder.add_fun<QFont, const QString &, const QString &, int, &QFontDatabase::font>("font");
   // bool isBitmapScalable(const QString &, const QString &) const;
@@ -137,7 +132,7 @@ static void register_font_database_class(script::Namespace ns)
   // static int addApplicationFontFromData(const QByteArray &);
   binder.add_static<int, const QByteArray &, &QFontDatabase::addApplicationFontFromData>("addApplicationFontFromData");
   // static QStringList applicationFontFamilies(int);
-  binder.add_static<QStringList, int, &QFontDatabase::applicationFontFamilies>("applicationFontFamilies");
+  /// TODO: static QStringList applicationFontFamilies(int);
   // static bool removeApplicationFont(int);
   binder.add_static<bool, int, &QFontDatabase::removeApplicationFont>("removeApplicationFont");
   // static bool removeAllApplicationFonts();
@@ -148,12 +143,15 @@ static void register_font_database_class(script::Namespace ns)
   binder.add_static<QFont, QFontDatabase::SystemFont, &QFontDatabase::systemFont>("systemFont");
 }
 
-void register_fontdatabase_file(script::Namespace root)
+
+void register_fontdatabase_file(script::Namespace gui)
 {
   using namespace script;
 
-  register_font_database_class(root);
-  binding::Namespace binder{ root };
+  Namespace ns = gui;
+
+  register_font_database_class(ns);
+  binding::Namespace binder{ ns };
 
 }
 
